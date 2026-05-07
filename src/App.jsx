081e05d550cf2f7e56667120bd28c02ed8d50482
Daniel Muthui <dannymuthui118@gmail.com>
2026-05-07 20:31:46 +0300

    feat: new expenseproject

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import {useState} from 'react'
+import Search from `./components/Search`;
+import ExForm from './components/ExForm';
+import ExTable from './components/ExTable'
+
+function App() {
+  return (
+    
+  )
+}
